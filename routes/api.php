bda22c94c8f2242edf5d4fcdaa2eee4ca548ab88
DedeSnrwn13 <sunarwandede13@gmail.com>
2025-05-30 12:36:15 +0700

    feat: fetch user api

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::prefix('products')->controller(ProductController::class)->group(function () {
     Route::get('/', 'all');
 });
@@ -20,3 +16,7 @@ Route::prefix('categories')->controller(ProductCategoryController::class)->group
 
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [UserController::class, 'fetch']);
+});
